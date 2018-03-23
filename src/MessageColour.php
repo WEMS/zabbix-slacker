f2afbfcc15fa94970e39e8cc2e265fe8104d977f
Ben Cromwell <bencromwell@wems.co.uk>
2018-03-23 08:41:36 +0000

    Additional notification colours from Zabbix UI

--- src/MessageColour.php
+++ src/MessageColour.php
@@ -4,16 +4,33 @@ namespace Wems\Zabbix;
 
 class MessageColour
 {
+    // some taken from Zabbix UI, some are Slack standards
+    const GOOD = 'good';
+    const INFO = '#7499FF';
+    const WARNING = 'warning';
+    const AVERAGE = '#FFA059';
+    const HIGH = '#E97659';
+    const DISASTER = 'danger';
+
     public function deriveFrom(Params $params): string
     {
         if ($params->TRIGGER_STATUS === 'OK') {
-            $colour = 'good';
-        } elseif (strtolower($params->TRIGGER_SEVERITY) === 'warning') {
-            $colour = 'warning';
-        } else {
-            $colour = 'danger';
+            return self::GOOD;
         }
 
-        return $colour;
+        switch (strtolower($params->TRIGGER_SEVERITY)) {
+            case 'information':
+                return self::INFO;
+            case 'warning':
+                return self::WARNING;
+            case 'average':
+                return self::AVERAGE;
+            case 'high':
+                return self::HIGH;
+            case 'disaster':
+                return self::DISASTER;
+            default:
+                return self::WARNING;
+        }
     }
 }
--- tests/MessageColourTest.php
+++ tests/MessageColourTest.php
@@ -5,20 +5,62 @@ use Wems\Zabbix\Params;
 
 class MessageColourTest extends \PHPUnit\Framework\TestCase
 {
-    public function testColour()
+    /** @var Params */
+    private $params;
+
+    /** @var MessageColour */
+    private $messageColour;
+
+    public function setUp()
     {
-        $params = new Params();
+        $this->params = new Params();
+        $this->messageColour = new MessageColour();
+    }
 
-        $messageColour = new MessageColour();
+    public function testOK()
+    {
+        $this->params->TRIGGER_STATUS = 'OK';
+        $this->assertColour(MessageColour::GOOD);
+    }
 
-        $params->TRIGGER_STATUS = 'OK';
-        $this->assertEquals('good', $messageColour->deriveFrom($params));
+    public function testInformation()
+    {
+        $this->params->TRIGGER_SEVERITY = 'Information';
+        $this->assertColour(MessageColour::INFO);
+    }
 
-        $params->TRIGGER_STATUS = 'BAD';
-        $params->TRIGGER_SEVERITY = 'Warning';
-        $this->assertEquals('warning', $messageColour->deriveFrom($params));
+    public function testWarning()
+    {
+        $this->params->TRIGGER_SEVERITY = 'Warning';
+        $this->assertColour(MessageColour::WARNING);
+    }
 
-        $params->TRIGGER_SEVERITY = 'HELL HAS BROKEN LOOSE';
-        $this->assertEquals('danger', $messageColour->deriveFrom($params));
+    public function testAverage()
+    {
+        $this->params->TRIGGER_SEVERITY = 'Average';
+        $this->assertColour(MessageColour::AVERAGE);
+    }
+
+    public function testHigh()
+    {
+        $this->params->TRIGGER_SEVERITY = 'High';
+        $this->assertColour(MessageColour::HIGH);
+    }
+
+    public function testDisaster()
+    {
+        $this->params->TRIGGER_SEVERITY = 'Disaster';
+        $this->assertColour(MessageColour::DISASTER);
+    }
+
+    public function testUnknown()
+    {
+        $this->params->TRIGGER_SEVERITY = 'FooBar';
+        $this->assertColour(MessageColour::WARNING);
+    }
+
+    private function assertColour(string $colour)
+    {
+        $this->assertEquals($colour, $this->messageColour->deriveFrom($this->params));
     }
 }
